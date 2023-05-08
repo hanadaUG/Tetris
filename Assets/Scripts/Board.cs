4348f1fb0de64a387dcb3e882c5ecc9d73360a66
hanadaUG <hanadaUG@gmail.com>
2023-05-08 21:05:02 +0900

    Board追加

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    [SerializeField]
+    private Transform emptySprite;
+    
+    [SerializeField]
+    private int height = 30, width = 10, header = 8;
+
+    private void Start()
+    {
+        CreateBoard();
+    }
+
+    void CreateBoard()
+    {
+        if (emptySprite)
+        {
+            for (int y = 0; y < height - header; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Transform clone = Instantiate(emptySprite, new Vector3(x, y, 0), Quaternion.identity);
+                    clone.transform.parent = transform;
+                }
+            }
+        }
+    }
+}
